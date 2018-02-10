57c4c9800c9ba02ed495a33192e635a5aa91756f
Jim Gumbley <jgumbley@thoughtworks.com>
2018-02-10 16:34:44 +0000

    cut of workshop materials, static

--- src/clj/ssc/website/website.clj
+++ src/clj/ssc/website/website.clj
@@ -1,5 +1,6 @@
 (ns ssc.website.website
   (:require [hiccup.page :refer [include-js include-css html5]]
+            [hiccup.element :refer [image]]
             [config.core :refer [env]]
             [hiccup.util :refer [escape-html]]))
 
@@ -8,6 +9,7 @@
 (defn head []
   [:head
    [:meta {:charset "utf-8"}]
+   [:meta {:description "Sensible Security Conversations is a workshop based approach to threat modelling focussed on agile software development teams and modern cloud-based digital systems."}]
    [:meta {:name    "viewport"
            :content "width=device-width, initial-scale=1"}]
    (include-css (if (env :dev) "/css/website.css" "/css/website.min.css"))
@@ -22,14 +24,16 @@
      (head)
      [:body {:class "body-container"}
       [:div.content
-       [:h1 "Sensible Security Conversations"]
-       [:p "Sensible Security Conversations is a workshop based approach to threat modelling focussed on agile software development teams and modern cloud-based digital systems."]
+       [:h1 [:image {:alt "Sensible Security Conversations"
+                     :src "https://raw.githubusercontent.com/ThoughtWorksInc/sensible-security-conversations/master/banner-logo.png" :width "670"}]]
+       [:p "<b>Sensible Security Conversations</b> is a workshop based approach to threat modelling focussed on agile software development teams and modern cloud-based digital systems."]
+       [:p "This site currently holds the key materials to conduct the workshops, but it is a work in progress. More details will be added over the coming weeks and months."]
        [:hr]
-       [:table
-        [:td "Assets"]
-        [:td "Threats"]
-        [:td "Exposure"]
-        ]
+       [:ul
+        [:li "Overview of approach as given at NCSC Developers Den seminar"]
+        [:li "PDF version of Threat cards for printing out on A4 paper"]
+        [:li "PDF version of Exposure cards for printing out on A4 paper"]
+       ]
        [:hr]
        [:table
         [:td "CC logo"]
